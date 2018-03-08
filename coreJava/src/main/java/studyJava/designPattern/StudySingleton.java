8ea205c24b2ca50b446fc4f58ed4928b08de7d07
zhuzhenyu <zhuzhenyu@utry.cn>
2018-03-08 16:06:53 +0800

    单例模式

--- coreJava/src/main/java/studyJava/designPattern/StudySingleton.java
+++ coreJava/src/main/java/studyJava/designPattern/StudySingleton.java
@@ -9,5 +9,68 @@ package studyJava.designPattern;
  * @version 1.0.0
  */
 public class StudySingleton {
-
+	
+	
+	private StudySingleton(){}
+	
+	private static StudySingleton singleton;
+	
+	/**
+	 * 并发有问题  懒汉加载 用的的时候才加载
+	 * @Description (TODO这里用一句话描述这个方法的作用)
+	 * @return
+	 */
+	public static StudySingleton getInsanceOne(){
+		if(singleton ==null){
+			singleton = new StudySingleton();
+		}
+		return singleton;
+	}
+	
+	/**
+	 * 	锁加的有问题
+	 * @Description (TODO这里用一句话描述这个方法的作用)
+	 * @return
+	 */
+	public static StudySingleton getInstanceTwo(){
+		if(singleton == null){
+			synchronized (StudySingleton.class) {
+				singleton = new StudySingleton();
+			}
+		}
+		return singleton;
+	}
+	
+	
+	/**
+	 * 锁范围太大
+	 * @Description (TODO这里用一句话描述这个方法的作用)
+	 * @return
+	 */
+	public static StudySingleton getInstanceThree(){
+		synchronized (StudySingleton.class) {
+			if(singleton == null ){
+				singleton = new StudySingleton();
+			}
+		}
+		return singleton;
+	}
+	
+	/**
+	 * 双重验证
+	 * @Description (TODO这里用一句话描述这个方法的作用)
+	 * @return
+	 */
+	public static StudySingleton getInstanceFour(){
+		if(singleton==null){
+			synchronized (StudySingleton.class) {
+				if(singleton == null ){
+					singleton = new StudySingleton();
+				}
+			}
+		}
+		
+		return singleton;
+	}
+	
 }
