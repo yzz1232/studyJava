0a0afce84af473d1132ad889b27a3922e83a5a55
zhuzhenyu <zhuzhenyu@utry.cn>
2018-03-08 11:44:36 +0800

    init

---
+++ coreJava/src/main/java/studyJava/coreJava/App.java
@@ -0,0 +1,13 @@
+package studyJava.coreJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ coreJava/src/main/java/studyJava/designPattern/StudySingleton.java
@@ -0,0 +1,13 @@
+package studyJava.designPattern;
+
+/**
+ * 单例模式
+ * @ClassName StudySingleton
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author 87593
+ * @Date 2018年3月8日 上午11:37:24
+ * @version 1.0.0
+ */
+public class StudySingleton {
+
+}
